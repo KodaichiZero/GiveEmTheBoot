8ce5e165b0ffbe06c30ccf8b7e96d912184f7736
Captain McScroggo <shutupyoufool@gmail.com>
2021-03-09 13:58:51 -0500

    first commit I think?

---
+++ Mod/Mod.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using BepInEx;
+using HarmonyLib;
+
+namespace GiveEmTheBoot
+{
+    [BepInPlugin("org.kodaichizero.giveemtheboot", "GiveEmTheBoot", "1.0.0.0")]
+    public class Mod : BaseUnityPlugin
+    {
+        private static readonly Harmony harmony = new(typeof(Mod).GetCustomAttributes(typeof(BepInPlugin), false)
+            .Cast<BepInPlugin>()
+            .First()
+            .GUID);
+
+        private void Awake()
+        {
+            harmony.PatchAll();
+        }
+
+        private void OnDestroy()
+        {
+            harmony.UnpatchSelf();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,2 @@
-# GiveEmTheBoot
 
 A valheim mod which empowers the unarmed kick attack. Knockback will send foes in an upward diagonal direction, dependent on their weight. Trolls will be slightly nudged, Greylings will be caber-tossed. Gets stronger with your unarmed skill, as well as the weight of your legwear. At 100 unarmed skill and with iron leg armor, a greyling will be yeeted 50+ feet away. It's the unarmed kick you always wanted.
