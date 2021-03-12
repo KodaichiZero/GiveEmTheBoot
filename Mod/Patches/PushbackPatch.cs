f70e206ff1b1e8a6eeab1f9a58ce9b43be008080
Captain McScroggo <shutupyoufool@gmail.com>
2021-03-12 15:55:35 -0500

    Deleted unnecessary stuff, added pushback bonus to the kick, cleaned up code a lot.

--- Mod/Patches/PushbackPatch.cs
+++ Mod/Patches/PushbackPatch.cs
@@ -1,77 +1,63 @@
-﻿using System;
+﻿using HarmonyLib;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Globalization;
-using System.Linq;
-using System.Reflection;
-using HarmonyLib;
-using GiveEmTheBoot.Utils;
 using UnityEngine;
 
-namespace GiveEmTheBoot.Patches
-{
-    [HarmonyPatch(typeof(Character), "ApplyPushback")]
-    public static class PushbackPatch
-    {
-        
-        public static void Postfix(ref Player __instance, HitData hit)
-        {
+namespace GiveEmTheBoot.Patches {
+	[HarmonyPatch(typeof(Character), "ApplyPushback")]
+	public static class PushbackPatch {
 
-            if (hit == null || !hit.HaveAttacker())
-            {
-                //No Hitdata found, exiting processing
-                return;
-            }
+		//In this method we check when pushback is being applied to an enemy, and if we're kicking them, we add force.
+		public static void Prefix(ref Character __instance, HitData hit) {
 
-            Character character = hit.GetAttacker();
+			//Make sure the hit is an attacking player.
+			if(!hit.HaveAttacker() || !(hit.GetAttacker() is Player)) {
+				return;
+			}
 
-            if(character == null || character.GetType() != typeof(Player))
-            {
-                //No processing needed if there is no attacker or it's not a player.
-                return;
-            }
+			Player player = (Player)hit.GetAttacker();
 
-            Player player = (Player)character;
+			//Check to see if the player is attacking using their foot.
+			if(player.m_currentAttack != null && player.m_currentAttack.m_attackAnimation == "unarmed_kick") {
+				float pushForce = 0f;
 
-            //Check to see if the player is attacking using their foot.
-            if(player.m_currentAttack != null && player.m_currentAttack.m_attackAnimation == "unarmed_kick")
-            {
-                float pushForce = 0f;
+				//Add in the player's unarmed level to the additional push force, max of 75F extra force at level 100.
+				if(player.GetSkills().m_skillData.ContainsKey(Skills.SkillType.Unarmed)) {
+					float level = player.GetSkills().m_skillData.GetValueSafe(Skills.SkillType.Unarmed).m_level;
+					pushForce += (level / 100F) * 75F;
+				}
 
-                //Add in the player's unarmed level to the additional push force.
-                if(player.GetSkills().m_skillData.ContainsKey(Skills.SkillType.Unarmed))
-                {
-                    pushForce += player.GetSkills().m_skillData.GetValueSafe(Skills.SkillType.Unarmed).m_level * 0.75F;
-                }
+				//Add the weight of the player's foot armor to the push force, max bonus of 50F with 15 pound boots.
+				List<ItemDrop.ItemData> equipList = new List<ItemDrop.ItemData>();
+				player.GetInventory().GetWornItems(equipList);
+				foreach(ItemDrop.ItemData item in equipList) {
+					if(item.GetTooltip().Contains("legs")) {
+						pushForce += (item.GetWeight() / 15F) * 75F;
+						break;
+					}
+				}
 
-                //Add the weight of the player's foot armor to the push force.
-                List<ItemDrop.ItemData> equipList = new List<ItemDrop.ItemData>();
-                player.GetInventory().GetWornItems(equipList);
-                foreach(ItemDrop.ItemData item in equipList)
-                {
-                    if (item.GetTooltip().Contains("legs"))
-                    {
-                        pushForce += item.GetWeight() * 5F;
-                        break;
-                    }
-                }
+				//Also add a flat 50F bonus if the enemy is staggered.
+				if(__instance.IsStaggering()) {
+					pushForce += 50F;
+				}
 
-                //Apply additional poush force to the hit data.
-                if(pushForce > 0f)
-                {
+				//Apply additional poush force to the hit data.
+				if(pushForce > 0f) {
 
-                    Vector2 horizontalVector = new Vector2(player.GetMoveDir().x, player.GetMoveDir().z);
-                    horizontalVector.Normalize();
-                    horizontalVector *= pushForce;
-                    Vector3 yeetVector = new Vector3(horizontalVector.x, pushForce * 0.375F, horizontalVector.y);
+					Vector2 horizVector = new Vector2(player.GetMoveDir().x, player.GetMoveDir().z);
+					horizVector.Normalize();
+					horizVector *= pushForce;
+					Vector3 fullVector = new Vector3(horizVector.x, pushForce * 0.375F, horizVector.y);
 
-                    //Add a large amount of force with mass taken into account
-                    __instance.GetComponent<Rigidbody>().AddForce(yeetVector, ForceMode.Impulse);
+					//Add a large amount of force with mass taken into account
+					//__instance.GetComponent<Rigidbody>().AddForce(yeetVector, ForceMode.Impulse);
 
-                    //Add a small amount of velocity regardless of mass
-                    __instance.GetComponent<Rigidbody>().AddForce(yeetVector * 0.1F, ForceMode.VelocityChange);
-                }
-            }
-        }
-    }
+					//Add a small amount of velocity regardless of mass
+					//__instance.GetComponent<Rigidbody>().AddForce(yeetVector * 0.1F, ForceMode.VelocityChange);
+
+					__instance.m_pushForce = fullVector;
+				}
+			}
+		}
+	}
 }
